775773ca74b32ff09645c64272b61bdb911ce296
Naimur00 <aminaim4u@gmail.com>
2024-11-19 11:56:55 +0600

    add: card

---
+++ app/components/Card.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Card = () => {
+  return (
+    <div className='relative bg-[url("@/deadpool-wolverine.jpg")] bg-cover bg-center h-64 w-full'>
+        <div className=''>
+            <h1>haha</h1>
+        </div>
+    </div>
+  )
+}
