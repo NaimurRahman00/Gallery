c60ad2e9d478cafcd3dbc2e6387c52954ca879c2
Naimur00 <aminaim4u@gmail.com>
2024-11-20 15:44:05 +0600

    update: onchange on input

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
-import { IoSearchOutline } from "react-icons/io5";
+import React, { useState } from 'react';
+import { IoSearchOutline } from 'react-icons/io5';
 
-export default function Input() {
+interface InputProps {
+  onSearch: (query: string) => void; // **New Prop**
+}
+
+export default function Input({ onSearch }: InputProps) { // **Added onSearch**
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setSearchValue(value);
+    onSearch(value); 
+  };
+
   return (
-    <div className='relative'>
-        <input className="w-80 bg-gradient-to-r from-blue-950/80 to-blue-900/80 rounded-full px-4 py-2 text-gray-300 capitalize focus:outline-none" type="text" placeholder='SEARCH' />
-        <span className='absolute top-2 right-3 text-gray-300 text-2xl font-bold cursor-pointer'><IoSearchOutline /></span>  
+    <div className="relative">
+      <input
+        className="w-80 bg-gradient-to-r from-blue-950/80 to-blue-900/80 rounded-full px-4 py-2 text-gray-300 capitalize focus:outline-none"
+        type="text"
+        placeholder="SEARCH"
+        value={searchValue}
+        onChange={handleChange}
+      />
+      <span className="absolute top-2 right-3 text-gray-300 text-2xl font-bold cursor-none">
+        <IoSearchOutline />
+      </span>
     </div>
-  )
+  );
 }
