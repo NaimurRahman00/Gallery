670213a7d2dbf182851fd53fe76eec4715fe83a7
Naimur00 <aminaim4u@gmail.com>
2024-11-20 15:42:36 +0600

    add: passing props through navbar and movies

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
+'use client';
 
+import { useState } from "react";
 import { Carousel } from "./carousels/Carousel";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Movies from "./ui/movies/Movies";
 
 export default function Home() {
+  const [searchQuery, setSearchQuery] = useState('');
+
   return (
     <div className="scroll-smooth">
       <div className="bg-gradient-to-br from-slate-700 to-[#1e2028]">
-        <Navbar />
+        <Navbar onSearch={setSearchQuery} />
         <Carousel/>
-        <Movies/>
+        <Movies searchQuery={searchQuery} />
         <Footer/>
       </div>
     </div>
