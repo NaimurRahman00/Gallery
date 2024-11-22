52b0f3e7fac0c0d7e713133030864ff56790032b
Naimur00 <aminaim4u@gmail.com>
2024-11-22 23:31:34 +0600

    responsive: footer

--- app/ui/movies/Footer.tsx
+++ app/ui/movies/Footer.tsx
@@ -60,7 +60,7 @@ function Footer() {
                         </div>
                     </div>
                     <div className="mt-6 lg:mt-0 lg:flex-1">
-                        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                        <div className="flex justify-between sm:grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                             <div className='text-sky-500'>
                                 <h3 className="uppercase text-white">Movies</h3>
                                 <a
