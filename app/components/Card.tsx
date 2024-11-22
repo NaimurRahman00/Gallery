6a45c740e77a95bf2d30580c6a16c342a0350495
Naimur00 <aminaim4u@gmail.com>
2024-11-22 23:38:06 +0600

    update: card

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -16,20 +16,20 @@ export const Card: React.FC<CardProps> = ({ name, poster, price }) => {
   return (
     <div>
       <div className="w-full max-w-[160px] space-y-3 rounded-md bg-white shadow-lg dark:bg-[#18181B] overflow-hidden group">
-        <div className="relative flex h-48 w-full justify-center lg:h-[260px]">
+        <div className="relative flex h-60 w-full justify-center lg:h-[260px]">
           {/* Price Tag */}
           <div className="absolute left-4 right-4 top-4 flex justify-between">
             <h6 className="rounded shadow-2xl shadow-black bg-sky-400 text-white text-xl px-2">
               <sup className="text-xs">$</sup>{price}
             </h6>
-            <span className="rounded shadow-2xl shadow-black bg-[#ffc107] px-2 py-0.5 h-fit font-bold text-xs text-black">
+            <span className="rounded shadow-xl shadow-black bg-[#ffc107] px-2 py-0.5 h-fit font-bold text-xs text-black">
               HD
             </span>
           </div>
 
           {/* Movie Title */}
-          <div className="absolute left-2 right-2 top-2 bottom-2 flex flex-col justify-end text-white font-bold leading-4 text-center">
-            <h1 className="bg-gradient-to-t from-black/70 to-black/0 pt-4">
+          <div className="absolute left-0 right-0 top-0 bottom-0 flex flex-col justify-end text-white font-bold leading-4 text-center">
+            <h1 className="bg-gradient-to-t from-black/80 to-black/0 pt-8 pb-3">
               {name}
             </h1>
           </div>
